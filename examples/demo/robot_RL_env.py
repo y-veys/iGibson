b67cd2c0331b911dbcfd3b8120848d2e51f88f64
veys <yveys@caltech.edu>
2020-07-01 10:52:32 -0700

    Changed config file

--- examples/demo/robot_RL_env.py
+++ examples/demo/robot_RL_env.py
@@ -11,14 +11,15 @@ import logging
 
 def main():
     config_filename = os.path.join(os.path.dirname(gibson2.__file__),
-                                   '../examples/configs/jr2.yaml')
-    nav_env = NavigateRandomEnv(config_file=config_filename, mode='gui')
+                                   '../examples/configs/jr2_reaching.yaml')
+    nav_env = NavigateEnv(config_file=config_filename, mode='pbgui')
 
     for j in range(10):
         nav_env.reset()
         for i in range(100):
             with Profiler('Environment action step'):
                 action = nav_env.action_space.sample()
+                print(action)
                 state, reward, done, info = nav_env.step(action)
                 if done:
                     logging.info("Episode finished after {} timesteps".format(i + 1))
